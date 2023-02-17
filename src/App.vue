c8d392e9bb61ca732776a057b9ba571f3078a326
Timur Nazarov <nazarovtim11@gmail.com>
2023-02-18 01:03:49 +0200

    1

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="wrapper">
+      <link href="https://fonts.cdnfonts.com/css/montserrat" rel="stylesheet">
+      <textarea  v-model="input"></textarea>
+      <div class="copy">{{ computedInput }}</div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      input: ''
+    }
+  },
+  computed: {
+    computedInput() {
+      return this.input.replaceAll('deep', '♂ deep ♂')
+    }
+  }
+}
+</script>
+
+<style>
+  .wrapper {
+    padding-top: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-family: 'Montserrat';
+  }
+  .copy {
+    background: rgba(0,0,0,.1);
+    width: 500px;
+    min-height: 200px;
+    margin-top: 20px;
+    padding: 10px;
+  }
+  textarea {
+    padding: 10px;
+    width: 500px;
+    resize: none;
+  }
+</style>
